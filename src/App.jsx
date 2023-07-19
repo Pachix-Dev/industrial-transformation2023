8a7ee5dff1eb47940412f86da899032c67b3e72e
JoseZR <josezerrod@hotmail.com>
2023-07-19 09:33:47 -0600

    Cambios en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useTranslation } from 'react-i18next'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
 import { Routes, Route } from 'react-router'
 import { Helmet } from 'react-helmet'
 import { Menu } from './components/Menu/Menu'
