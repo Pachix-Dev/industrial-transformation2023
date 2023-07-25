6e132ec5174088d6433980d9c22f6869fe8628ab
Pachi-Dev <pachi.claros@gmail.com>
2023-07-25 12:29:20 -0600

    fix counter in IOS

--- src/components/Home/CounterDown.jsx
+++ src/components/Home/CounterDown.jsx
@@ -1,64 +1,54 @@
-import React, { useEffect, useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
+import { useCountdown } from '../hooks/useCountDown'
 
 export function CounterDown () {
   const { t } = useTranslation()
+  const [days, hours, minutes, seconds] = useCountdown('2023-10-04')
 
-  const calculateTimeLeft = () => {
-    const year = new Date().getFullYear()
-    const difference = +new Date(`${year}-10-4`) - +new Date()
-    let timeLeft = {}
+  if (days + hours + minutes + seconds <= 0) {
+    return <span>Time's up!</span>
+  } else {
+    return (
+      <Container className='mt-2 mb-5 text-center home-counterdown-timer'>
+        <Row className='counterdown-timer pb-3'>
+          <Col md={3} xs={6}>
+            <div className='timer-numbers'>
+              <span>{days}</span>
+            </div>
+            <p className='timer-text'>
+              {t('home.days')}
+            </p>
+          </Col>
+          <Col md={3} xs={6}>
+            <div className='timer-numbers'>
+              <span>{hours}</span>
+            </div>
+            <p className='timer-text'>
+              {t('home.hours')}
+            </p>
+          </Col>
+          <Col md={3} xs={6}>
+            <div className='timer-numbers'>
+              <span>{minutes}</span>
+            </div>
+            <p className='timer-text'>
+              {t('home.minutes')}
+            </p>
+          </Col>
+          <Col md={3} xs={6}>
+            <div className='timer-numbers'>
+              <span>{seconds}</span>
+            </div>
+            <p className='timer-text'>
+              {t('home.seconds')}
+            </p>
+          </Col>
 
-    if (difference > 0) {
-      timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60)
-      }
-    }
-
-    return timeLeft
-  }
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
-  const [year] = useState(new Date().getFullYear())
-
-  useEffect(() => {
-    setTimeout(() => {
-      setTimeLeft(calculateTimeLeft())
-    }, 1000)
-  })
-
-  const timerComponents = []
-
-  Object.keys(timeLeft).forEach((interval) => {
-    if (!timeLeft[interval]) {
-      return
-    }
-
-    timerComponents.push(
-      <Col md={3} xs={6} key={interval}>
-        <div className='timer-numbers'>
-          <span>{timeLeft[interval]}</span>
-        </div>
-        <p className='timer-text'>
-          {interval === 'days' ? t('home.days') : ''}
-          {interval === 'hours' ? t('home.hours') : ''}
-          {interval === 'minutes' ? t('home.minutes') : ''}
-          {interval === 'seconds' ? t('home.seconds') : ''}
-        </p>
-      </Col>
+        </Row>
+        <h1 className='fw-bold mb-5' style={{ color: 'white' }}>Days before ITM 2023</h1>
+        <a className='home-register' href='https://itm.ahmreg.com.mx/' target='_blank' rel='noreferrer'>{t('home.registro_itm_2023')}</a>
+      </Container>
     )
-  })
-  return (
-    <Container className='mt-2 mb-5 text-center home-counterdown-timer'>
-      <Row className='counterdown-timer pb-3'>
-        {timerComponents.length ? timerComponents : <span>Time's up!</span>}
-      </Row>
-      <h1 className='fw-bold mb-5' style={{ color: 'white' }}>Days before ITM {year}</h1>
-
-      <a className='home-register' href='https://itm.ahmreg.com.mx/' target='_blank' rel='noreferrer'>{t('home.registro_itm_2023')}</a>
-    </Container>
-  )
+  }
 }
