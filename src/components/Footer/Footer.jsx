d2959a799bd15cde66194fd2530328aae2559a35
Pachi-Dev <pachi.claros@gmail.com>
2023-08-14 16:51:11 -0600

    fix acento mexico

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -45,7 +45,7 @@ export function Footer () {
           <Col md={4}>
             <div className='footer-wrapper-info-contact'>
               <div className='w-100'>
-                <p><strong>Industrial Transformation México 2023</strong></p>
+                <p><strong>Industrial Transformation Mexico 2023</strong></p>
                 <p>{t('footer.dateFooter')}<br />
                   9:00 am – 6:00 pm<br />
                   <svg width={18} viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'><g id='SVGRepo_bgCarrier' strokeWidth='0' /><g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round' /><g id='SVGRepo_iconCarrier'> <path d='M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z' stroke='#000000' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' /> <path d='M12 13C13.6569 13 15 11.6569 15 10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10C9 11.6569 10.3431 13 12 13Z' stroke='#000000' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' /> </g></svg> Poliforum León, Guanajuato
