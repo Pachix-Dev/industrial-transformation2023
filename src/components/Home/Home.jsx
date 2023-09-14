da9018953cd230d3e124160e268689114da10f0f
Pachi-Dev <pachi.claros@gmail.com>
2023-09-14 17:37:04 -0600

    fix logos space

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -310,11 +310,11 @@ export function Home () {
           </Col>
 
           <Col md={3} className='mx-auto'>
-            <Col md={3} className='mx-auto'>
-              <a href='https://heraldodemexico.com.mx/' target='_blank' rel='noreferrer'>
-                <img src='/heraldo.webp' width={200} loading='lazy' alt='Heraldo' />
-              </a>
-            </Col>
+            <a href='https://heraldodemexico.com.mx/' target='_blank' rel='noreferrer'>
+              <img src='/heraldo.webp' width={200} loading='lazy' alt='Heraldo' />
+            </a>
+          </Col>
+          <Col md={3} className='mx-auto'>
             <a href='https://www.am.com.mx/' target='_blank' rel='noreferrer'>
               <img src='/am.webp' width={200} loading='lazy' alt='AM ' />
             </a>
