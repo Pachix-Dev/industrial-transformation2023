8a037dea716e9d59bd1bb08c0fddd03adffa31bd
Pachi-Dev <pachi.claros@gmail.com>
2024-01-18 14:57:43 -0600

    Refactor Toolkit component to use correct gallery images

--- src/components/Toolkit/Toolkit.jsx
+++ src/components/Toolkit/Toolkit.jsx
@@ -3,13 +3,15 @@ import { Gallery } from '../Gallery/Gallery'
 import { toolkitES, toolkitEN, toolkitNames } from '../constants_gallery'
 import { Container } from 'react-bootstrap'
 import './Toolkit.css'
-export function Toolkit () {
+export function Toolkit() {
   const { t, i18n } = useTranslation()
   return (
     <Container className='mt-5 mb-5'>
-      <h1 className='text-center fw-bold mb-5' style={{ color: '#e8001e' }}>{t('toolkit.title')}</h1>
+      <h1 className='text-center fw-bold mb-5' style={{ color: '#e8001e' }}>
+        {t('toolkit.title')}
+      </h1>
       <Gallery
-        galleryImgs={i18n.language === 'en' ? toolkitES : toolkitEN}
+        galleryImgs={i18n.language === 'en' ? toolkitEN : toolkitES}
         galleryName='ITM-TOOLKIT'
         toolkitNamesImg={toolkitNames}
       />
