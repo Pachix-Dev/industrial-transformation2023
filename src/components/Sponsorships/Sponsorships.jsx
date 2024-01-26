a9b1caff96c710b82033b8106464131b2166f39d
Pachi-Dev <pachi.claros@gmail.com>
2024-01-26 12:26:50 -0600

    Update Sponsorships component with correct file links

--- src/components/Sponsorships/Sponsorships.jsx
+++ src/components/Sponsorships/Sponsorships.jsx
@@ -1,7 +1,7 @@
 import { Card, Col, Container, Row } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
 
-export function Sponsorships () {
+export function Sponsorships() {
   const { t } = useTranslation()
   return (
     <>
@@ -13,8 +13,16 @@ export function Sponsorships () {
             <Card>
               <Card.Img variant='top' src='/sponsorbg1.webp' />
               <Card.Body>
-                <Card.Title className='mb-3'>Paquetes de Patrocinios: Platino, Gold y Silver</Card.Title>
-                <a href='/files/1-Patrocinio-Alianza-ITM-2024' target='_blank' className='bg-primary text-white rounded p-2 text-decoration-none'>Leer más</a>
+                <Card.Title className='mb-3'>
+                  Paquetes de Patrocinios: Platino, Gold y Silver
+                </Card.Title>
+                <a
+                  href='/files/1-Patrocinio-Alianza-ITM-2024.pdf'
+                  target='_blank'
+                  className='bg-primary text-white rounded p-2 text-decoration-none'
+                >
+                  Leer más
+                </a>
               </Card.Body>
             </Card>
           </Col>
@@ -22,8 +30,16 @@ export function Sponsorships () {
             <Card>
               <Card.Img variant='top' src='/sponsorbg2.webp' />
               <Card.Body>
-                <Card.Title className='mb-3'>Patrocinio de Conferencia Keynote</Card.Title>
-                <a href='/files/2-Patrocinio-Keynote-ITM-2024.pdf' target='_blank' className='bg-primary text-white rounded p-2 text-decoration-none'>Leer más</a>
+                <Card.Title className='mb-3'>
+                  Patrocinio de Conferencia Keynote
+                </Card.Title>
+                <a
+                  href='/files/2-Patrocinio-Keynote-ITM-2024.pdf'
+                  target='_blank'
+                  className='bg-primary text-white rounded p-2 text-decoration-none'
+                >
+                  Leer más
+                </a>
               </Card.Body>
             </Card>
           </Col>
@@ -31,8 +47,16 @@ export function Sponsorships () {
             <Card>
               <Card.Img variant='top' src='/sponsorbg3.webp' />
               <Card.Body>
-                <Card.Title className='mb-3'>Patrocinios Noche de Industriales</Card.Title>
-                <a href='/files/3-Patrocinios-Noche-de-Industriales-ITM-2024.pdf' target='_blank' className='bg-primary text-white rounded p-2 text-decoration-none'>Leer más</a>
+                <Card.Title className='mb-3'>
+                  Patrocinios Noche de Industriales
+                </Card.Title>
+                <a
+                  href='/files/3-Patrocinios-Noche-de-Industriales-ITM-2024.pdf'
+                  target='_blank'
+                  className='bg-primary text-white rounded p-2 text-decoration-none'
+                >
+                  Leer más
+                </a>
               </Card.Body>
             </Card>
           </Col>
@@ -40,8 +64,16 @@ export function Sponsorships () {
             <Card>
               <Card.Img variant='top' src='/sponsorbg4.webp' />
               <Card.Body>
-                <Card.Title className='mb-3'>Patrocinio presentación comercial – Transformation Area</Card.Title>
-                <a href='/files/4-Patrocinio-Transformation-ITM-2024.pdf' target='_blank' className='bg-primary text-white rounded p-2 text-decoration-none'>Leer más</a>
+                <Card.Title className='mb-3'>
+                  Patrocinio presentación comercial – Transformation Area
+                </Card.Title>
+                <a
+                  href='/files/4-Patrocinio-Transformation-ITM-2024.pdf'
+                  target='_blank'
+                  className='bg-primary text-white rounded p-2 text-decoration-none'
+                >
+                  Leer más
+                </a>
               </Card.Body>
             </Card>
           </Col>
@@ -49,8 +81,16 @@ export function Sponsorships () {
             <Card>
               <Card.Img variant='top' src='/sponsorbg5.webp' />
               <Card.Body>
-                <Card.Title className='mb-3'>Patrocinio de Módulos de registro</Card.Title>
-                <a href='/files/5-Patrocinio-Módulos-de-registro-ITM-2024.pdf' target='_blank' className='bg-primary text-white rounded p-2 text-decoration-none'>Leer más</a>
+                <Card.Title className='mb-3'>
+                  Patrocinio de Módulos de registro
+                </Card.Title>
+                <a
+                  href='/files/5-Patrocinio-Módulos-de-registro-ITM-2024.pdf'
+                  target='_blank'
+                  className='bg-primary text-white rounded p-2 text-decoration-none'
+                >
+                  Leer más
+                </a>
               </Card.Body>
             </Card>
           </Col>
@@ -58,8 +98,16 @@ export function Sponsorships () {
             <Card>
               <Card.Img variant='top' src='/sponsorbg6.webp' />
               <Card.Body>
-                <Card.Title className='mb-3'>Patrocinio Inauguración</Card.Title>
-                <a href='/files/6-Patrocinio-Inauguracion-ITM-2024.pdf' target='_blank' className='bg-primary text-white rounded p-2 text-decoration-none'>Leer más</a>
+                <Card.Title className='mb-3'>
+                  Patrocinio Inauguración
+                </Card.Title>
+                <a
+                  href='/files/6-Patrocinio-Inauguracion-ITM-2024.pdf'
+                  target='_blank'
+                  className='bg-primary text-white rounded p-2 text-decoration-none'
+                >
+                  Leer más
+                </a>
               </Card.Body>
             </Card>
           </Col>
@@ -67,8 +115,16 @@ export function Sponsorships () {
             <Card>
               <Card.Img variant='top' src='/sponsorbg7.webp' />
               <Card.Body>
-                <Card.Title className='mb-3'>Patrocinio Área de Networking</Card.Title>
-                <a href='/files/7-Patrocinio-Área-de-Networking-ITM-2023.pdf' target='_blank' className='bg-primary text-white rounded p-2 text-decoration-none'>Leer más</a>
+                <Card.Title className='mb-3'>
+                  Patrocinio Área de Networking
+                </Card.Title>
+                <a
+                  href='/files/7-Patrocinio-Área-de-Networking-ITM-2023.pdf'
+                  target='_blank'
+                  className='bg-primary text-white rounded p-2 text-decoration-none'
+                >
+                  Leer más
+                </a>
               </Card.Body>
             </Card>
           </Col>
