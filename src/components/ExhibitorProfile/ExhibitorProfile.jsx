d333132c7ebd67ca8feaa0e776a17bcb4849d3c3
Jesus Zermeño <josezerrod@hotmail.com>
2024-03-15 06:12:14 -0600

    se agrega topicos de segmentos de interes

--- src/components/ExhibitorProfile/ExhibitorProfile.jsx
+++ src/components/ExhibitorProfile/ExhibitorProfile.jsx
@@ -2,7 +2,7 @@ import './ExhibitorProfile.css'
 import { useTranslation } from 'react-i18next'
 import eder from '../../assets/ederRangel.webp'
 import { ProductsCategory } from '../ProductsCategory/ProductsCategory'
-import { Col, Row } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 export function ExhibitorProfile() {
   const { t, i18n } = useTranslation()
   return (
@@ -17,9 +17,9 @@ export function ExhibitorProfile() {
                 className='exhibitor-description'
                 dangerouslySetInnerHTML={{ __html: t('profile.description') }}
               />
-              <div className='home-files-download gap-5'>
+              <div className='home-files-download gap-3 row row-cols-auto justify-content-center align-items-start' >
                 <a
-                  className='home-register'
+                  className='home-register col text-center mb-3'
                   href={
                     i18n.language === 'en'
                       ? '/files/Factsheet-ITM-2024-ENG-V8.pdf'
@@ -28,7 +28,7 @@ export function ExhibitorProfile() {
                   target='_blank'
                   rel='noreferrer'
                 >
-                  <span className='d-md-flex justify-content-evenly fw-bold'>
+                  <span className='d-flex justify-content-evenly align-items-center fw-bold'>
                     Factsheet 2024
                     <svg
                       xmlns='http://www.w3.org/2000/svg'
@@ -47,12 +47,12 @@ export function ExhibitorProfile() {
                   </span>
                 </a>
                 <a
-                  className='home-register'
+                  className='home-register col text-center mb-3'
                   href='/files/Floorplan_ITM_2024_08.03.2024.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
-                  <span className='d-md-flex justify-content-evenly fw-bold'>
+                  <span className='d-flex justify-content-evenly align-items-center fw-bold'>
                     {t('menu.exhibitors_5')}
                     <svg
                       xmlns='http://www.w3.org/2000/svg'
@@ -71,12 +71,12 @@ export function ExhibitorProfile() {
                   </span>
                 </a>
                 <a
-                  className='home-register'
+                  className='home-register col text-center mb-3'
                   href='/files/ITM2024-contract-V2.pdf'
                   target='_blank'
                   rel='noreferrer'
                 >
-                  <span className='d-md-flex justify-content-evenly fw-bold'>
+                  <span className='d-flex justify-content-evenly align-items-center fw-bold'>
                     {t('menu.exhibitors_6')}
                     <svg
                       xmlns='http://www.w3.org/2000/svg'
@@ -94,8 +94,8 @@ export function ExhibitorProfile() {
                     </svg>
                   </span>
                 </a>
-                <a className='home-register' href='/sponsorships'>
-                  <span className='d-md-flex justify-content-evenly fw-bold'>
+                <a className='home-register col text-center' href='/sponsorships'>
+                  <span className='d-flex justify-content-evenly align-items-center fw-bold'>
                     {t('menu.exhibitors_7')}
                   </span>
                 </a>
@@ -104,7 +104,71 @@ export function ExhibitorProfile() {
           </Row>
         </div>
       </div>
-      {/* <ProductsCategory /> */}
+      <Container className='my-5'>
+            <h1 className='fs-1 fw-bold text-center text-uppercase' style={{color:'#DD0000'}}>{t("profile.why-exhibit.title")}</h1>
+            <Row className='gap-5 mt-5'>
+              <Col sm  className='text-center'>
+                  <div className='' style={{color:'#DD0000'}}> 
+                    <p className='fs-2 fw-bold text-uppercase'><span style={{fontSize:'70px'}}>15,000</span> <br /><span className='text-black'>
+                      {t("profile.why-exhibit.description-1")}</span> <br /> <span className='fs-1 fw-bold'>{t("profile.why-exhibit.description-2")} </span> </p>
+                    <img src="/countries.webp" alt="" className="img-fluid" width='300'/>
+                  </div>
+              </Col>
+              <Col sm className='text-center'>
+                  <div  style={{color:'#DD0000'}}>
+                    <p className='fs-2 fw-bold text-uppercase'><span className='fs-1'></span> <br /> {t("profile.why-exhibit.description-3")} </p>
+                    <img src="/attendees.webp" alt="" className="img-fluid"  width='350'/>
+                  </div>
+              </Col>
+            </Row>
+      </Container>
+      <Container  className='fs-5 fw-semibold mt-5 pt-3' style={{color:'#DD0000'}}>
+        <h1 className='fs-1 fw-bold text-center text-uppercase my-5'>{t("profile.segments.title")}</h1>
+        <Row className='gap-5 '>
+          <Col className="columna text-center d-flex flex-column justify-content-start align-items-center">
+            <img src="/segment_icon_1.webp" alt="Imagen 1" className="imagen" width={150} />
+            <p className="texto">{t("profile.segments.item-1")}</p>
+          </Col>
+          <Col className="columna text-center d-flex flex-column justify-content-start align-items-center">
+            <img src="/segment_icon_2.webp" alt="Imagen 2" className="imagen" width={150} />
+            <p className="texto">{t("profile.segments.item-2")}</p>
+          </Col>
+          <Col className="columna text-center d-flex flex-column justify-content-start align-items-center">
+            <img src="/segment_icon_3.webp" alt="Imagen 3" className="imagen" width={150} />
+            <p className="texto">{t("profile.segments.item-3")}</p>
+          </Col>
+          <Col className="columna text-center d-flex flex-column justify-content-start align-items-center">
+            <img src="/segment_icon_4.webp" alt="Imagen 4" className="imagen" width={150} />
+            <p className="texto">{t("profile.segments.item-4")}</p>
+          </Col>
+          <Col className="columna text-center d-flex flex-column justify-content-start align-items-center">
+            <img src="/segment_icon_5.webp" alt="Imagen 5" className="imagen" width={150} />
+            <p className="texto">{t("profile.segments.item-5")}</p>
+          </Col>
+        </Row>
+        <Row className='gap-5 pt-4'>
+          <Col className="columna text-center d-flex flex-column justify-content-start align-items-center">
+            <img src="/segment_icon_6.webp" alt="Imagen 1" className="imagen" width={150} />
+            <p className="texto">{t("profile.segments.item-6")}</p>
+          </Col>
+          <Col className="columna text-center d-flex flex-column justify-content-start align-items-center">
+            <img src="/segment_icon_7.webp" alt="Imagen 2" className="imagen" width={150} />
+            <p className="texto">{t("profile.segments.item-7")}</p>
+          </Col>
+          <Col className="columna text-center d-flex flex-column justify-content-start align-items-center">
+            <img src="/segment_icon_8.webp" alt="Imagen 3" className="imagen" width={150} />
+            <p className="texto">{t("profile.segments.item-8")}</p>
+          </Col>
+          <Col className="columna text-center d-flex flex-column justify-content-start align-items-center">
+            <img src="/segment_icon_9.webp" alt="Imagen 4" className="imagen" width={150} />
+            <p className="texto">{t("profile.segments.item-9")}</p>
+          </Col>
+          <Col className="columna text-center d-flex flex-column justify-content-start align-items-center">
+            <img src="/segment_icon_10.webp" alt="Imagen 5" className="imagen" width={150} />
+            <p className="texto">{t("profile.segments.item-10")}</p>
+          </Col>
+        </Row>
+      </Container>
       <div className='pt-5 pb-5 text-black text-center'>
         <div className='border-bottom border-2 border-light' />
         <h4 className='mt-3 mb-3'>{t('exhibitorReasons.become_exhibitor')}</h4>
